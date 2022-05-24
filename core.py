036ef987cd56b2ebd61837bd39a1f54b8f5d74e9
dimcheck <dfarinyak@gmail.com>
2022-05-24 17:38:40 +0300

    structuringWIP

--- core.py
+++ core.py
@@ -8,10 +8,12 @@ from helpers import get_marketnews, get_google_img
 API_KEY = os.environ['API_KEY']
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
+
 # Self-explanatory.
 @bot.message_handler(commands=['help'])
 def send_explanation(message):
     bot.send_message(message.chat.id, 'Enter a ticker like TRX-USD or AAPL to see their current price and info.')
+
 
 # Self-explanatory.
 @bot.message_handler(commands=['start'])
@@ -20,18 +22,18 @@ def send_welcome(message):
     menu_1 = types.InlineKeyboardButton('Top-10 crypto🪙', callback_data='open_crypto_list')
     menu_2 = types.InlineKeyboardButton('Top-10 corporations🏦', callback_data='open_corpo_list')
     menu_3 = types.InlineKeyboardButton('Latest News', callback_data='open_news_list')
-    menu_4 = types.InlineKeyboardButton('About', callback_data='open_about')
-    
+    # menu_4 = types.InlineKeyboardButton('About', callback_data='open_about')
     main_keyboard.add(menu_1, menu_2, menu_3)
     bot.send_message(
-        message.chat.id, 
-        'Hello! \n' + 
-        'This is a currency market cap bot based on yahoo db!\n' +
+        message.chat.id,
+        'Hello! \n'
+        'This is a currency market cap bot based on yahoo db!\n'
         'Type /help for more info.',
         reply_markup=main_keyboard)
 
+
 # Sends base answers for buttun prompts.
-@bot.callback_query_handler(func=lambda call:True)
+@bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     if call.data == "main_menu":
         main_keyboard = types.InlineKeyboardMarkup(row_width=1)
@@ -40,10 +42,14 @@ def callback(call):
         menu_3 = types.InlineKeyboardButton('Latest News', callback_data='open_news_list')
 
         main_keyboard.add(menu_1, menu_2, menu_3)
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="Main Menu",reply_markup=main_keyboard)
-    
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text="Main Menu",
+            reply_markup=main_keyboard)
+
     if call.data == "open_news_list":
-        for link in get_marketnews():    
+        for link in get_marketnews():
             bot.send_message(call.message.chat.id, link)
 
     if call.data == 'open_corpo_list':
@@ -61,19 +67,26 @@ def callback(call):
         back = types.InlineKeyboardButton(text='Back', callback_data='main_menu')
 
         keyboard.add(
-            item_1, item_2, item_3, 
-            item_4, item_5, item_6, 
-            item_7, item_8, item_9, 
+            item_1, item_2, item_3,
+            item_4, item_5, item_6,
+            item_7, item_8, item_9,
             item_10, back)
-        
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text='Top-10 corporations🏦',reply_markup=keyboard)
+            
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text='Top-10 corporations🏦',
+            reply_markup=keyboard)
 
     elif call.data == 'AAPL':
         currency = yf.Ticker(f"{call.data}")
         image = f'{call.data}'
         info = currency.info['longBusinessSummary']
         print(info)
-        price = currency.info['shortName'] + " : " + str(currency.info['regularMarketPrice']) + '💸\n' + '24H📈 : ' + str(currency.info['dayHigh']) + '$\n' + '24H📉 : ' + str(currency.info['dayLow']) + '$\n'
+        price = currency.info['shortName'] + " : "
+        + str(currency.info['regularMarketPrice']) + '💸\n'
+        + '24H📈 : ' + str(currency.info['dayHigh']) + '$\n' + '24H📉 : ' 
+        + str(currency.info['dayLow']) + '$\n'
         bot.send_photo(call.message.chat.id, get_google_img(image), caption=price)
         bot.send_message(call.message.chat.id, info)
 
@@ -173,12 +186,16 @@ def callback(call):
         back = types.InlineKeyboardButton(text='Back', callback_data='main_menu')
 
         keyboard.add(
-            item_1, item_2, item_3, 
-            item_4, item_5, item_6, 
-            item_7, item_8, item_9, 
+            item_1, item_2, item_3,
+            item_4, item_5, item_6,
+            item_7, item_8, item_9,
             item_10, back)
         
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text='Top-10 crypto🪙',reply_markup=keyboard)
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text='Top-10 crypto🪙',
+            reply_markup=keyboard)
        
     elif call.data == 'btc-usd':
         currency = yf.Ticker(f"{call.data}")
@@ -259,5 +276,6 @@ def callback(call):
         price = currency.info['shortName'] + " : " + str(currency.info['regularMarketPrice']) + '💸\n' + '24H📈 : ' + str(currency.info['dayHigh']) + '$\n' + '24H📉 : ' + str(currency.info['dayLow']) + '$\n'
         bot.send_photo(call.message.chat.id, get_google_img(image), caption=price)
         bot.send_message(call.message.chat.id, info)
+
 
 bot.infinity_polling()
