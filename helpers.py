82ecfe6bb557bb13a83cbbdcd78f8b637aea8f54
dimcheck <dfarinyak@gmail.com>
2022-05-26 18:01:26 +0300

    moving to OOP

--- helpers.py
+++ helpers.py
@@ -9,6 +9,7 @@ def get_google_img(query: 'str') -> 'str':
     headers = {'content-type': 'image/png'}
     response = requests.get(url, headers=headers).text
     soup = BeautifulSoup(response, 'html.parser')
+
     items = soup.find_all('img', {'class': 'yWs4tf'})
     index = random.randint(0, 5)
     dirty_image = items[index]
@@ -16,20 +17,26 @@ def get_google_img(query: 'str') -> 'str':
 
 
 # Gets latest news from yahoo marketnews.
-def get_marketnews():
-    # response = requests.get('https://finance.yahoo.com/topic/stock-market-news/')
-    response = requests.get('https://finance.yahoo.com/topic/crypto/')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    base_page = soup.find('ul', {'class': 'My(0) P(0) Wow(bw) Ov(h)'})
-    # news_page = soup.find('ul', {'class': 'My(0) P(0) Wow(bw) Ov(h)'}).find_all('p')
-    # title_page = soup.find('ul', {'class': 'My(0) P(0) Wow(bw) Ov(h)'}).find_all('a')
-    # part_url = 'https://finance.yahoo.com/topic/stock-market-news'
-    part_url = 'https://finance.yahoo.com/topic/crypto'
+class News:
+    tag = 'ul'
+    nav_str = {'class': 'My(0) P(0) Wow(bw) Ov(h)'}
+    base_url = 'https://finance.yahoo.com/topic/crypto/'
+    full_link_url = 'https://finance.yahoo.com/topic/crypto/{path_to_article}'
 
-    if base_page := base_page.find_all(href=True):
-        for link in base_page:
-            full_url = part_url + link['href']
-            yield full_url
+    def __call__(self):
+        soup = self._get_client()
+        base_page = soup.find(self.tag, self.nav_str)
+        return self.iterate_links(base_page)
+
+    def _get_client(self) -> None:
+        response = requests.get(self.base_url)
+        return BeautifulSoup(response.content, 'html.parser')
+
+    def iterate_links(self, base_page):
+        if base_page := base_page.find_all(href=True):
+            for link in base_page:
+                yield self.full_link_url.format(path_to_article=link['href'])
+
 
 # coin = "BTC-USD"
 # currency = yf.Ticker(f"{coin}")
